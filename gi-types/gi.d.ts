bc53e8eef6f6e706145243099be21595094dd3c2
Angelo Verlain <geoangercola@gmail.com>
2023-05-05 11:12:15 +0200

    fix gen-gi

--- gi-types/gi.d.ts
+++ gi-types/gi.d.ts
@@ -1,20 +1,20 @@
 declare module "gi://Vips" {
-  export * as default from "vips";
+  export * as default from "gi-types/vips8";
 }
 declare module "gi://Vips?version=8" {
-  export * as default from "vips";
+  export * as default from "gi-types/vips8";
 }
 
 declare module "gi://GObject" {
-  export * as default from "gobject";
+  export * as default from "gi-types/gobject2";
 }
 declare module "gi://GObject?version=2" {
-  export * as default from "gobject";
+  export * as default from "gi-types/gobject2";
 }
 
 declare module "gi://GLib" {
-  export * as default from "glib";
+  export * as default from "gi-types/glib2";
 }
 declare module "gi://GLib?version=2" {
-  export * as default from "glib";
+  export * as default from "gi-types/glib2";
 }
