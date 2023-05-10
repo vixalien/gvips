e6d027f3fc8c831bf5605c3b12eea300ce40a2d5
Angelo Verlain <geoangercola@gmail.com>
2023-05-10 11:49:57 +0200

    add static test

--- test/main.ts
+++ test/main.ts
@@ -5,12 +5,14 @@ import { initWrappers } from "src/wrapper";
 
 initWrappers();
 
-const image = Vips.Image.new_from_file_RW(
-  "/home/alien/Pictures/Exports/6 April 2023/sdffff.jpg",
-) as any;
+const copy = Vips.Image.black(100, 100);
 
-const copy = image.colourspace(Vips.Interpretation.B_W) as Vips.Image;
+// const image = Vips.Image.new_from_file_RW(
+//   "/home/alien/Pictures/Exports/6 April 2023/sdffff.jpg",
+// ) as any;
+
+// const copy = image.colourspace(Vips.Interpretation.B_W) as Vips.Image;
 
 // const copy = op.call([image, Vips.Interpretation.B_W]) as Vips.Image;
 
-console.log(vips_image_write_to_file(copy, "test.jpg"));
+console.log(vips_image_write_to_file(copy, "test.png"));
